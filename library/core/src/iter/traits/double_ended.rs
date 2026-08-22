c30a30e9ad280ba68eed255d31eaef1c44fe86c9
Evgenii Zheltonozhskii <zheltonozhskiy@gmail.com>
2026-08-22 18:30:06 +0300

    remove the last rustc_non_const_trait_method

--- library/core/src/iter/traits/double_ended.rs
+++ library/core/src/iter/traits/double_ended.rs
@@ -133,12 +133,12 @@ pub const trait DoubleEndedIterator: [const] Iterator {
     /// ```
     #[inline]
     #[unstable(feature = "iter_next_chunk", issue = "98326")]
-    #[rustc_non_const_trait_method]
     fn next_chunk_back<const N: usize>(
         &mut self,
     ) -> Result<[Self::Item; N], array::IntoIter<Self::Item, N>>
     where
         Self: Sized,
+        Self::Item: [const] Destruct,
     {
         crate::array::iter_next_chunk_back(self)
     }
